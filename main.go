5cf74f9b28a9a9410b0998dad0fa4a9ae6c1a4f7
Abhishek <ksabhishek00@gmail.com>
2025-03-10 20:50:33 +0530

    variables, string and numbers

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+
+func main(){
+
+	var word string = "Hello World"
+	newName := "Hello World"
+	var age  int = 20
+	newAge := 20
+	var price float32 = 20.5
+	var newPrice float64 = 22220000.322222
+	fmt.Println(word,newName,age,newAge, price,newPrice)
+}
