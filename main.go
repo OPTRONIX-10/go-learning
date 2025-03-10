2fde94194958a2a477649a0ba802d7650a31b47f
Abhishek <ksabhishek00@gmail.com>
2025-03-10 21:17:30 +0530

    printing methods

--- main.go
+++ main.go
@@ -3,12 +3,32 @@ import "fmt"
 
 
 func main(){
+	// numbers, variables, strings
 
-	var word string = "Hello World"
-	newName := "Hello World"
-	var age  int = 20
-	newAge := 20
-	var price float32 = 20.5
-	var newPrice float64 = 22220000.322222
-	fmt.Println(word,newName,age,newAge, price,newPrice)
+	// var word string = "Hello World"
+	// newName := "Hello World"
+	// var age  int = 20
+	// newAge := 20
+	// var price float32 = 20.5
+	// var newPrice float64 = 22220000.322222
+	// fmt.Println(word,newName,age,newAge, price,newPrice)
+
+	// printing methods
+
+	// age := 20.555
+	// name := "John"
+	// fmt.Print("My age is",age)
+	// fmt.Print("My name is",name)
+	// fmt.Println("My age is",age)
+	// fmt.Println("My name is",name)
+	// fmt.Printf("My age is %f  and my name is %s\n",age,name)
+	// var word = fmt.Sprintf("My age is %f  and my name is %s",age,name)
+	// fmt.Println(word)
+
+	//arrays, alices
+
+	
+
+
+
 }
