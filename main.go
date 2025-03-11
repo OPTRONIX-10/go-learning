6c0a8e49234df30e6ca4e143305d5e591739759a
Abhishek <ksabhishek00@gmail.com>
2025-03-11 20:50:08 +0530

    maps

--- main.go
+++ main.go
@@ -2,49 +2,21 @@ package main
 
 import (
 	"fmt"
-	"strings"
-	// "sort"
-	// "strings"
+	
 )
 
 
-func sample (names []string, fn func(string)){
-	for _,valus :=range names{
-		fn(valus)
-	}
-
-}
-
-func greeting(name string){
-	fmt.Println("Hello", name)
-	
-}
-
-func returnName(name1 string, name2 string)string{
-	return name1 + " " + name2
-}
-
-func returnTwoStrings(name string)(string, string){
-	s := strings.ToUpper(name)
-	names := strings.Split(s, " ")
-	var nameSlice []string
-	for _,val :=range names{
-		nameSlice = append(nameSlice, val[:1])
-	}
-	if len(nameSlice) > 1{
-		return nameSlice[0], nameSlice[1]
-	}
-	return nameSlice[0], "_"
-}
 func main(){
-	greeting("john")
-	sample([]string{"john","doe","jane"}, greeting)
-	name := returnName("john", "doe")
-	f1,f2 := returnTwoStrings("john doe")
-	
-	fmt.Println(name, f1, f2)
-	//scope of pacakge
-	sampleNames("john")
-
-
+ nameAges := map[string]int{
+	"James": 32,
+	"Jill": 40,
+	"Jack": 23,
+	"Jenny": 26,
+ }
+ fmt.Print(nameAges)
+ for k,v := range nameAges{
+	fmt.Print(k," ",v)
+ }
+ nameAges["James"] = 33
+ fmt.Println(nameAges)
 }
