ee8524a3476b34e16ada8c7ad60025bb57657448
Abhishek <ksabhishek00@gmail.com>
2025-03-11 20:30:30 +0530

    go functions

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	// "sort"
 	// "strings"
 )
@@ -22,11 +23,25 @@ func greeting(name string){
 func returnName(name1 string, name2 string)string{
 	return name1 + " " + name2
 }
+
+func returnTwoStrings(name string)(string, string){
+	s := strings.ToUpper(name)
+	names := strings.Split(s, " ")
+	var nameSlice []string
+	for _,val :=range names{
+		nameSlice = append(nameSlice, val[:1])
+	}
+	if len(nameSlice) > 1{
+		return nameSlice[0], nameSlice[1]
+	}
+	return nameSlice[0], "_"
+}
 func main(){
 	greeting("john")
 	sample([]string{"john","doe","jane"}, greeting)
 	name := returnName("john", "doe")
-	fmt.Println(name)
+	f1,f2 := returnTwoStrings("john doe")
+	fmt.Println(name, f1, f2)
 
 
 }
