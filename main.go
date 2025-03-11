914fb07be517536b76606fcd6cc0098a5768ac14
Abhishek <ksabhishek00@gmail.com>
2025-03-11 21:16:05 +0530

    pointers

--- main.go
+++ main.go
@@ -5,18 +5,13 @@ import (
 	
 )
 
-
+func updateString(s *string){
+	*s = "Hello"
+}
 func main(){
- nameAges := map[string]int{
-	"James": 32,
-	"Jill": 40,
-	"Jack": 23,
-	"Jenny": 26,
- }
- fmt.Print(nameAges)
- for k,v := range nameAges{
-	fmt.Print(k," ",v)
- }
- nameAges["James"] = 33
- fmt.Println(nameAges)
+ name := "Abhishek"
+ newPointer := &name
+ fmt.Println("Name: ", name)
+ updateString(newPointer)
+ fmt.Println("Name: ", name)
 }
